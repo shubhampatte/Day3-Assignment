069d0a8c0d9b5ffd8331fc16b9708eb377ffa1ea
shubhampatte <shubhampatte@gmail.com>
2021-09-30 14:01:18 +0530

    [shubham] EmpWage on master

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,20 +1,10 @@
-public class EmployeeWage {
+public class EmployeeWage
+{
+	public static void main(String args[])
+	{
 
-	public static void main(String[] args) {
-		int IS_PRESENT = 1;
-		int EMP_WAGE_PER_HR = 20;
-		double attendance =Math.floor(Math.random() * 10) % 2;
-		if (attendance == IS_PRESENT) {
-			System.out.println("employee is present");
-			int WORKING_HRS = 8;
-			int EmpWage = WORKING_HRS * EMP_WAGE_PER_HR;
-			System.out.println(EmpWage);
-		}
-		else {
-			System.out.println("employee is absent");
-		}
+		System.out.println("Welcome to Employee Wage Computation Program");
 	}
 
 }
-
  
