234386af23123557104f6542d1ff437a443f6fe8
shubhampatte <shubhampatte@gmail.com>
2021-10-01 12:35:42 +0530

    [shubham]: Changes on master

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,7 +2,7 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 
-		System.out.println("Employee Wage Program for month");
+		System.out.println("Employee Wage Master");
 	}
 
 
