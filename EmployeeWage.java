5ff045275d96d86d786dc4b241444e9117a92799
shubhampatte <shubhampatte@gmail.com>
2021-10-01 18:28:50 +0530

    [shubham]: changes on master

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,7 +2,7 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 
-		System.out.println("Employee Wage Master");
+		System.out.println(" EmployeeWage Problem for Totaldays and Max Hrs");
 	}
 
 
